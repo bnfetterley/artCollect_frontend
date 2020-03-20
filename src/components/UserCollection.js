c0f5c25e166319bafd6f51d2533d125d2aaa13e4
Bri Fetterley <bnfetterley@usfca.edu>
2020-03-19 20:31:31 -0400

    auth is working

--- src/components/UserCollection.js
+++ src/components/UserCollection.js
@@ -1,40 +1,60 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect } from 'react'
 import { MDBContainer, MDBRow, MDBCol } from "mdbreact"
 import '../index.css'
 import Modal from './Modal/Modal'
+import { useSelector, useDispatch } from 'react-redux'
+import actions from '../redux/actions';
+
+// export default class UserCollection extends Component {
+
+  const UserCollection = (props) => {
+
+    const username = useSelector(state => state.username
+    )
+
+    const login = useSelector(state => state)
 
 
-export default class UserCollection extends Component {
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+      if (localStorage.token) {
+        dispatch(actions.persistUser())
+      }
+    })
+
+    const userPosts = props.posts.filter(post => post.user_id === login.id)
+
+    // console.log(state.query)
 
 
-
-    render() {
-      let id = this.props.currentUserID
-        // let postsToRender = this.props.posts.filter(post => post.user_id === this.props.currentUserID)
-        let renderComments = this.props.comments.filter(comment => comment.post_id === this.props.currentArtwork.id)
-        let view = this.props.toggleUpdateForm === 'view'
-        let addCollectionView = this.props.toggleUpdateCollection === "view"
-  console.log(this.props)
+    // render() {
+      let id = props.currentUserID
+        // let postsToRender = props.posts.filter(post => post.user_id === props.currentUserID)
+        let renderComments = props.comments.filter(comment => comment.post_id === props.currentArtwork.id)
+        let view = props.toggleUpdateForm === 'view'
+        let addCollectionView = props.toggleUpdateCollection === "view"
+  // console.log(username, login)
         return (
 
         
 
 <div className="container">
 
-{/* { this.props.isShowing ? <div onClick={this.props.closeModalHandler} className="back-drop"></div> : null } */}
+{/* { props.isShowing ? <div onClick={props.closeModalHandler} className="back-drop"></div> : null } */}
 
 
-{this.props.isShowing ? 
-
+{props.isShowing ? 
+ 
 <Modal
-    currentArtwork = {this.props.currentArtwork}
+    currentArtwork = {props.currentArtwork}
     className="modal"
-    show={this.props.isShowing}
-    close={this.props.closeModalHandler}>
+    show={props.isShowing}
+    close={props.closeModalHandler}>
         <div> SHOW INFO <br></br>
 
-        <img src = {this.props.currentArtwork.image} className="image fit-image" />
-           <p> Artist:  {this.props.currentArtwork.artist}, {this.props.currentArtwork.artwork_title}
+        <img src = {props.currentArtwork.image} className="image fit-image" />
+           <p> Artist:  {props.currentArtwork.artist}, {props.currentArtwork.artwork_title}
 
            <div className = "contentDiv">
                 <p>
@@ -42,40 +62,40 @@ export default class UserCollection extends Component {
                 </p>
 
                 <p> 
-                  {this.props.currentArtwork.post_content} 
+                  {props.currentArtwork.post_content} 
                 </p>
                 </div>
             </p> 
     
             
 
-               <button onClick = {this.props.toggleUpdateForm} >Share what you think about this piece!</button> <br></br>
+               <button onClick = {props.toggleUpdateForm} >Share what you think about this piece!</button> <br></br>
 
 
-               { this.props.toggleUpdateFormShow ?  (
+               { props.toggleUpdateFormShow ?  (
                
-                <form onSubmit={(event) => this.props.submitUpdate(event, this.state)}>
+                <form onSubmit={(event) => props.submitUpdate(event)}>
                
-               <textarea name = "newPostContent" value={this.props.newPostContent} onChange={(event) => this.props.handleChange(event)} />
+               <textarea name = "newPostContent" value={props.newPostContent} onChange={(event) => props.handleChange(event)} />
 
                 <input type="submit" value="Submit" />
                </form>)            
                
                : null }
 
-              <button onClick = {(event) => this.props.deletePost(event, this.state)}>delete this post</button> <br></br>
+              <button onClick = {(event) => props.deletePost(event)}>delete this post</button> <br></br>
   
 
             <p> <b>COMMENTS: </b> </p>
 
-            {this.renderComments}
+            {renderComments}
                {(renderComments[1])} 
                {renderComments.map(comment => <p> {comment.content} </p>)}
         
-            <form onSubmit={(event) => this.props.submitComment(event, this.state)}>
+            <form onSubmit={(event) => props.submitComment(event)}>
         <label>
          What do you think? Add a comment below!<p>
-          <textarea value={this.props.comment} onChange={(event) => this.handleChange(event)} name = "comment" />
+          {/* <textarea value={props.comment} onChange={(event) => handleChange(event)} name = "comment" /> */}
           </p> </label> 
 
         <input type="submit" value="Submit" />
@@ -87,26 +107,26 @@ export default class UserCollection extends Component {
 
 
 <MDBContainer>
-<a className="link dim gray b f1 f-headline-ns tc db mb3 mb4-ns" href="#" title="Home"> <header> <h1 key={this.props.currentUsername}> {this.props.currentUsername}'s collection</h1>  </header></a>
+<a className="link dim gray b f1 f-headline-ns tc db mb3 mb4-ns" href="#" title="Home"> <header> <h1 key={props.currentUsername}> {username}'s collection</h1>  </header></a>
 
 {/* <p>{ currentUser && currentUsername }</p> */}
 
-<button onClick = {this.props.toggleUpdateCollectionForm }> Add to your collection! </button>  <button onClick = {this.props.redirect }> Explore! </button>
+<button onClick = {props.toggleUpdateCollectionForm }> Add to your collection! </button>  <button onClick = {(event) => props.redirect (event, login) }> Explore! </button>
 
 {/* / form for adding to collection */}
 
-{ this.props.toggleUpdateCollectionShow ?  (     
-               <form onSubmit={(event) => this.props.submitNewPost(event, this.state)}>
+{ props.toggleUpdateCollectionShow ?  (     
+               <form onSubmit={(event) => props.submitNewPost(event)}>
 
-              <textarea name = "newImage" value = {this.props.newImage}onChange={(event) => this.props.handleChange(event)}  /> 
+              <textarea name = "newImage" value = {props.newImage}onChange={(event) => props.handleChange(event)}  /> 
               <br></br>
-              <textarea name = "newArtworkTitle"  value = {this.props.newArtworkTitle} onChange={(event) => this.props.handleChange(event)}  />
+              <textarea name = "newArtworkTitle"  value = {props.newArtworkTitle} onChange={(event) => props.handleChange(event)}  />
               <br></br>
-              <textarea name = "newArtist"  value = {this.props.newArtist} onChange={(event) => this.props.handleChange(event)}  />
+              <textarea name = "newArtist"  value = {props.newArtist} onChange={(event) => props.handleChange(event)}  />
               <br></br>
-              <textarea name = "newGenre" value = {this.props.newGenre}onChange={(event) => this.props.handleChange(event)}  />
+              <textarea name = "newGenre" value = {props.newGenre}onChange={(event) => props.handleChange(event)}  />
               <br></br>
-              <textarea name = "newPostContent"  value = {this.props.newPostContent} onChange={(event) => this.props.handleChange(event)} />
+              <textarea name = "newPostContent"  value = {props.newPostContent} onChange={(event) => props.handleChange(event)} />
               <br></br>
               <input type="submit" value="Submit" />
               </form>)            
@@ -115,8 +135,10 @@ export default class UserCollection extends Component {
 
 
       <MDBRow>
-        {this.props.posts.map(post =>  
-      <MDBCol size="6" md={this.props.size}  key = {post.id}  onClick = {(event) => this.props.openModalHandler(event, post)}>
+
+        
+        {userPosts.map(post =>  
+      <MDBCol size="6" md={props.size}  key = {post.id}  onClick = {(event) => props.openModalHandler(event, post)}>
         <img src = {post.image} className="img-responsive fit-image"/>
         </MDBCol> 
 
@@ -132,4 +154,7 @@ export default class UserCollection extends Component {
 </div>
 
 ) }
-}
+
+
+
+export default UserCollection
