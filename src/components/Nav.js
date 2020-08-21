8db91f582cfaaa3a38608df1397a21ae97c7a292
Bri Fetterley <bnfetterley@usfca.edu>
2020-08-20 20:49:48 -0400

    cleaning up code

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import  React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import userActions from '../redux/actions';
@@ -9,21 +9,22 @@ const Nav = () => {
     dispatch(userActions.logoutUser());
   };
   return (
-      <Fragment>
-   <header style={{ display: 'flex', justifyContent: 'center' }}> <h1> artCollect </h1>  </header>
-   <br>
-   </br>
-    <nav style={{ display: 'flex', justifyContent: 'space-evenly' }}>
-       
-      <Link to="/">Home</Link>
-      <Link to="/signup">Signup</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/" onClick={handleLogout}>Logout</Link>
-      <Link to="/usercollection" 
-    //   onClick = {this.closeModalHandler}
-      > User Collection</Link>
-    </nav>
- </Fragment>
+    <Fragment>
+      <header style={{ display: 'flex', justifyContent: 'center' }}>
+        {' '}
+        <h1> artCollect </h1>{' '}
+      </header>
+      <br></br>
+      <nav style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+        <Link to="/">Home</Link>
+        <Link to="/signup">Signup</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/" onClick={handleLogout}>
+          Logout
+        </Link>
+        <Link to="/usercollection"> User Collection</Link>
+      </nav>
+    </Fragment>
   );
 };
 
